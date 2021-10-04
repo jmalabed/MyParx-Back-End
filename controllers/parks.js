78b976b0baae5a1db12502189aa7b31511388005
Jess LaFrank <jesslafrank@gmail.com>
2021-10-04 10:06:53 -0700

    Added controller and linked to server

---
+++ controllers/parks.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+
+// -- index --
+router.get('/', (req, res) => {
+  res.send(';D')
+})
+
+// -- show --
+
+
+// -- create --
+
+
+// -- read --
+
+
+// -- update --
+
+
+module.exports = router
