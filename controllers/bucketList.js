eee3938ade086067884f33cb2c15818e32c4df24
Jess LaFrank <jesslafrank@gmail.com>
2021-10-04 11:02:36 -0700

    Added CREATE route for bucketList, updated INDEX route for bucketList

--- controllers/bucketList.js
+++ controllers/bucketList.js
@@ -4,8 +4,8 @@ const BucketList = require('../models/bucketList');
 // -- index --
 router.get('/', async (req, res) => {
   try {
-    const bucketItem = BucketList.create(req.body)
-    res.status(200).json(bucketItem)
+    const bucketItems = BucketList.find()
+    res.status(200).json(bucketItems)
   } catch (error) {
     res.status(400).json({ error: error.message })
   }
@@ -15,7 +15,14 @@ router.get('/', async (req, res) => {
 
 
 // -- create --
-
+router.post('/', async (req, res) => {
+  try {
+    const newBucketItem = BucketList.create(req.body)
+    res.status(200).json(newBucketItem)
+  } catch(error) {
+    res.status(400).json({ error: error.message })
+  }
+})
 
 // -- destory --
 
