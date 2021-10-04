7ba6997c346e1753d4d25a1b673b5bd306a33175
Jess LaFrank <jesslafrank@gmail.com>
2021-10-04 16:25:39 -0700

    Updated server

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ require('./db/db')
 const PORT = process.env.PORT || 9000
 const bucketListController = require('./controllers/bucketList')
 const packingListItemController = require('./controllers/packingListItem')
+const packingListController = require('./controllers/packingList')
 
 
 app.use(express.json())
 app.use('/bucketList', bucketListController)
 app.use('/packingListItem', packingListItemController)
+app.use('/packingList', packingListController)
 
 
 app.listen(PORT, () => console.log('listening on port', PORT))
