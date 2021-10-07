f57463e3aa22f21140140d021c0f1318a1db3710
jmalabed <jmalabed27@gmail.com>
2021-10-07 08:47:36 -0700

    updated server whitelist

--- server.js
+++ server.js
@@ -11,7 +11,11 @@ const packingListItemController = require("./controllers/packingListItem");
 const packingListController = require("./controllers/packingList");
 
 // Cors
-const whitelist = ["http://localhost:3000"];
+const whitelist = [
+  "http://localhost:3000",
+  "https://myparx.surge.sh",
+  "https://project-two-backend.herokuapp.com",
+];
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1 || !origin) {
