b21e6a3f7ebcab48996ca8b414c3b720fe5b3356
Michael Hamilton <hamiltonisaiah9119@gmail.com>
2021-10-06 20:27:54 -0700

    visited

--- models/bucketList.js
+++ models/bucketList.js
@@ -4,7 +4,7 @@ const falseBool = { type: Boolean, default: false }
 const bucketListSchema = mongoose.Schema({
     name: { type: String, required: true },
     isFav: falseBool,
-    vistied: falseBool,
+    visited: falseBool,
     date: { type: Date, default: Date.now },
     people: [String],
     dogFriendly: falseBool
