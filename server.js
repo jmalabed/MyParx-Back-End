a9da5ba6aa4bf52bbac034a26756dc1e0f0edc44
Michael Hamilton <hamiltonisaiah9119@gmail.com>
2021-10-07 09:10:03 -0700

    whiteList array

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const packingListItemController = require("./controllers/packingListItem");
 const packingListController = require("./controllers/packingList");
 
 // Cors
-const whitelist = ["http://localhost:3000"]
+const whitelist = ["http://localhost:3000", "https://myparx.surge.sh/", "https://project-two-backend.herokuapp.com"]
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1 || !origin) {
