8f3bc324cffeab78351420f27f2ba28c97f9436f
jmalabed <jmalabed27@gmail.com>
2021-10-10 14:20:17 -0700

    updated backend

--- db/db.js
+++ db/db.js
@@ -1,15 +1,18 @@
-const mongoose = require('mongoose')
+require("dotenv").config();
+const mongoose = require("mongoose");
+const DB_URL = process.env.DB_URL || "mongodb://localhost:27017/bucketList";
 
 // Error / Disconnection
-mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
-mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
-
+mongoose.connection.on("error", (err) =>
+  console.log(err.message + " is Mongod not running?")
+);
+mongoose.connection.on("disconnected", () => console.log("mongo disconnected"));
 
 // Connection string (we will be replacing this later with environmental variables)
-mongoose.connect('mongodb://localhost:27017/bucketList', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-mongoose.connection.once('open', ()=>{
-    console.log('connected to mongoose...')
-})
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.once("open", () => {
+  console.log("connected to mongoose...");
+});
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // dependencies
+require("dotenv").config();
 const express = require("express");
 const app = express();
 require("./db/db");
